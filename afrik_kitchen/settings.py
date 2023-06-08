45e574e055b89fc1eab9213ee493e3e1ba41473b
Adimora Uju Louisa <adimorauju@gmail.com>
2023-06-08 23:05:41 +0000

    create model for food menu list

--- afrik_kitchen/settings.py
+++ afrik_kitchen/settings.py
@@ -68,7 +68,7 @@ ROOT_URLCONF = 'afrik_kitchen.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR ],
+        'DIRS': ['TEMPLATES_DIR'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- view_menu/models.py
+++ view_menu/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+
+class FoodMenu(models.Model):
+    """ Model to create the menu list"""
+    name = models.CharField(max_length=45)
+    description = models.TextField()
+    price = models.FloatField(default=0.00)
+    image = models.ImageField(upload_to='media/',blank=True, null=True)
+
+
+class Meta:
+    """ Order by name"""
+    ordering = ['name']
+
+def __str__ (self):
+    return str(self.name)
