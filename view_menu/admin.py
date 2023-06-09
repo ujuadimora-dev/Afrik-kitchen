5f032041a89791e78230ccd4153a71a306eac767
Adimora Uju Louisa <adimorauju@gmail.com>
2023-06-09 01:17:12 +0000

    create model menu continue and registe in Admin panel

--- view_menu/admin.py
+++ view_menu/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import FoodMenu, Menu
+
+
+
+#admin.site.register(FoodMenu)
 
 # Register your models here.
+
+@admin.register(FoodMenu)
+class FoodMenuAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'price', 'image')
+    list_filter = ('name',)
+
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ('name', 'active', 'created_by', 'created_on', 'updated_on')
+    list_filter = ('name',)
+
+
+    def get_items(self, obj):
+        """ Function to return all menu items in many to many """
+        return "\n, ".join([m._food_name for m in obj.food_menu.all()])
+
+        #return "\n, ".join([m._item_name for m in obj.menu_items.all()])
+
+
--- view_menu/models.py
+++ view_menu/models.py
@@ -2,6 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+# Choice fields
+ITEM_TYPES = (
+    ("breakfast", "Breakfast"),
+    ("lunch", "Lunch"),
+    ("dinner", "Dinner"),
+    ("drink", "Drink"),
+    ("side", "Side")
+)
 
 
 class FoodMenu(models.Model):
@@ -15,6 +23,29 @@ class FoodMenu(models.Model):
 class Meta:
     """ Order by name"""
     ordering = ['name']
+
+def __str__ (self):
+    return str(self.name)
+
+
+class Menu(models.Model):
+    """ Model to create Menu"""
+    name = models.CharField(max_length=45)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="created")
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=False)
+    breakfasts = models.ManyToManyField('FoodMenu', related_name='breafasts')
+    lunchs = models.ManyToManyField('FoodMenu', related_name='lunchs')
+    dinners = models.ManyToManyField('FoodMenu', related_name='dinners')
+    drinks = models.ManyToManyField('FoodMenu', related_name='drinks')
+    sides = models.ManyToManyField('FoodMenu', related_name='sides')
+
+
+class Meta:
+    """order by active and name"""
+    ordering = ['-active', 'name']
+
 
 def __str__ (self):
     return str(self.name)
