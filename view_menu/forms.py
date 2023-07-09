b08c0c872143bcb1de8b3cd6625536d58aad89ab
Adimora Uju Louisa <adimorauju@gmail.com>
2023-07-09 20:26:51 +0000

    Refactor the forms.py for view_menu app

--- view_menu/forms.py
+++ view_menu/forms.py
@@ -7,6 +7,7 @@ MENU_CHOICES = [
     ('SIDE', 'Side'),
 ]
 
+
 class MenuForm(forms.Form):
     menu_type = forms.ChoiceField(
         choices=MENU_CHOICES,
@@ -17,84 +18,51 @@ class MenuForm(forms.Form):
     )
     
     description = forms.CharField(widget=forms.Textarea)
-    #fimage = forms.ImageField()
     fimage = forms.URLField()
     name = forms.CharField(max_length=100)
     priceM = forms.DecimalField(max_digits=6, decimal_places=2)
     priceL = forms.DecimalField(max_digits=6, decimal_places=2)
     vegetarian = forms.BooleanField(required=False)
     calorie_count = forms.IntegerField()
-
-
-class MenuCreateForm(forms.Form):
-    menu_type = forms.ChoiceField(
-        choices=MENU_CHOICES,
-        widget=forms.RadioSelect,
-        error_messages={
-            'required': 'Please select a menu type.',
-        }
-    )
-    description = forms.CharField(widget=forms.Textarea)
-    fimage = forms.URLField()
-    #image = forms.ImageField()
-    name = forms.CharField(max_length=100)
-    priceM = forms.DecimalField(max_digits=5, decimal_places=2)
-    priceL = forms.DecimalField(max_digits=5, decimal_places=2)
-    vegetarian = forms.BooleanField(required=False)
-    calorie_count = forms.IntegerField()
     created_on = forms.DateField()
     created_by_staff = forms.CharField(max_length=25)
 
     def save(self):
-        # Process the form data
+        """ Process the form data"""
         menu_type = self.cleaned_data['menu_type']
-        
+
         if menu_type == 'BREAKFAST':
-            breakfast = BreakFast.objects.create(
-                description=self.cleaned_data['description'],
-                fimage=self.cleaned_data['fimage'],
-                name=self.cleaned_data['name'],
-                priceM=self.cleaned_data['priceM'],
-                priceL=self.cleaned_data['priceL'],
-                vegetarian=self.cleaned_data['vegetarian'],
-                calorie_count=self.cleaned_data['calorie_count']
-               # created_on=self.cleaned_data['created_on'],
-                # created_by_staff=self.cleaned_data['created_by_staff']
-            )
-        
-            
+            return BreakFast.objects.create(**self.cleaned_data)
+
         elif menu_type == 'LUNCH':
-            lunch = Lunch.objects.create(
-                description=self.cleaned_data['description'],
-                fimage=self.cleaned_data['fimage'],
-                name=self.cleaned_data['name'],
-                priceM=self.cleaned_data['priceM'],
-                priceL=self.cleaned_data['priceL'],
-                vegetarian=self.cleaned_data['vegetarian'],
-                calorie_count=self.cleaned_data['calorie_count']
-            )
-            
-            
+            return Lunch.objects.create(**self.cleaned_data)
+
         elif menu_type == 'DINNER':
-            dinner = Dinner.objects.create(
-                description=self.cleaned_data['description'],
-                fimage=self.cleaned_data['fimage'],
-                name=self.cleaned_data['name'],
-                priceM=self.cleaned_data['priceM'],
-                priceL=self.cleaned_data['priceL'],
-                vegetarian=self.cleaned_data['vegetarian'],
-                calorie_count=self.cleaned_data['calorie_count']
-            )
-           
-            
+            return Dinner.objects.create(**self.cleaned_data)
+
         elif menu_type == 'SIDE':
-            sides = Sides.objects.create(
-                description=self.cleaned_data['description'],
-                fimage=self.cleaned_data['fimage'],
-                name=self.cleaned_data['name'],
-                priceM=self.cleaned_data['priceM'],
-                priceL=self.cleaned_data['priceL'],
-                vegetarian=self.cleaned_data['vegetarian'],
-                calorie_count=self.cleaned_data['calorie_count']
-            )
-           
+            return Sides.objects.create(**self.cleaned_data)
+
+        return None
+
+
+class MenuCreateForm(MenuForm):
+    created_on = forms.DateField()
+    created_by_staff = forms.CharField(max_length=25)
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+    
