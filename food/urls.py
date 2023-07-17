0310a32dd594f4899ea45b42690cd812455d1986
Adimora Uju Louisa <adimorauju@gmail.com>
2023-07-17 09:10:54 +0000

    Add template for creating menuitem, logic and style

--- food/urls.py
+++ food/urls.py
@@ -1,15 +1,12 @@
 from django.urls import path
 from . import views
 
-
-
-
 app_name = 'food'
 
 urlpatterns = [
     path('', views.food, name='food'),
-
     path('detail/<int:pk>/', views.detail, name='detail'),
-    path("add/", views.create_menuitem, name=create_menuitem)
-  
+    # path('menu_list/', views.food, name='menu_list'),
+
+    path('add/', views.create_menuitem, name='create_menuitem'),
 ]
--- food/views.py
+++ food/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import MenuItem
+from .forms import MenuitemDetail
+
 
 # Create your views here.
 
@@ -15,3 +17,13 @@ def food(request):
 def detail(request, pk):
     menuitem = MenuItem.objects.get(pk=pk)
     return render(request, 'food/detail.html', {'menuitem': menuitem})
+
+
+def create_menuitem(request):
+        form = MenuitemDetail(request.POST or None)
+
+        if form.is_valid():
+            form.save()
+            return redirect('food:food')  # Redirect to menu_list view
+    
+        return render(request, 'food/create_menu.html', {'form': form})
