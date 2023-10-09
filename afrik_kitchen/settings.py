34326a82dcf273092bb3d818e804bbca073bd53a
Adimora Uju Louisa <adimorauju@gmail.com>
2023-10-09 20:35:12 +0000

    fixing  errors

--- afrik_kitchen/settings.py
+++ afrik_kitchen/settings.py
@@ -67,8 +67,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'cloudinary_storage',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
+    
 
     # Django Allauth
 
